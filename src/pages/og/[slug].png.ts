a88d60b257ad9af89c626f12651f9c8ac55764d7
Matt Franchi <mattwfranc@gmail.com>
2025-06-29 09:52:08 -0400

    native sharing test

--- src/pages/og/[slug].png.ts
+++ src/pages/og/[slug].png.ts
@@ -2,6 +2,15 @@ import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
 import { generateOgImageForPost } from "@utils/generateOgImages";
 
+export async function getStaticPaths() {
+  const posts = await getCollection("blog");
+  
+  return posts.map((post) => ({
+    params: { slug: post.slug },
+    props: { post },
+  }));
+}
+
 export const GET: APIRoute = async ({ params }) => {
   try {
     const { slug } = params;
